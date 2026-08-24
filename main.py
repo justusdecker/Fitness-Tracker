29bcf279343a0d479ee15604cf4cd8a73181302a
Justus <justus.d2025@gmail.com>
2026-08-24 20:23:32 +0200

    add create endpoint

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from src.common.build_flask import *
-from src.backend.databases.data_access import DAH
+from src.backend.databases.data_access import DAH, Item
 for i in range(5):
     DAH.createItem(
         **{
-            'title': 'test_object' + str(i)
+            'title': 'test_object' + str(i),
+            'img': 'https://www.vitaminexpress.org/_next/image?url=https%3A%2F%2Fimages.cdn.europe-west1.gcp.commercetools.com%2F783def08-dd2b-475d-b671-c397c0c2dbd7%2F6958-04-L-Arginin_70-SjmjxvAb.png&w=1440&q=80'
         }
     )
 def err_return(code: int = 404):
@@ -14,7 +15,9 @@ def err_return(code: int = 404):
 
 @app.route('/',methods = [GET])
 def index():
-    return render_template('default.html', items = DAH.readItems())
-
+    return render_template('default.html', items = DAH.readItems(), Item = Item)
+@app.route('/create',methods = [GET])
+def create():
+    return render_template('create.html', Item = Item)
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
