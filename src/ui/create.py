4e59fbaafadd5c5851879eca26ddc29d0c18bb8d
Justus <justus.d2025@gmail.com>
2026-08-27 18:48:58 +0200

    refac and add createAndLeftPage

--- src/ui/create.py
+++ src/ui/create.py
@@ -1,15 +1,17 @@
 from src.ui.ui import UI
-from src.databases.items import Item
+from src.databases.data_access import Item, DAH
 import flet as ft
 class CreateItemUI(UI):
-    def __init__(self, page_switch):
+    def __init__(self, page_switch, items_ui):
         super().__init__()
-        self.textfields = []
+        self.items_ui = items_ui
+        self.page_switch = page_switch
+        self.textfields: list[ft.TextField | ft.Button] = []
         for key in Item.getVarTable():
             self.textfields.append(ft.TextField(label=key))
         
         self.textfields.append(
-            ft.Button(content=ft.Text('Erstellen'), icon=ft.Icons.CREATE, on_click=page_switch)
+            ft.Button(content=ft.Text('Erstellen'), icon=ft.Icons.CREATE, on_click=self.createAndLeftPage)
             )
         
         
@@ -25,6 +27,20 @@ class CreateItemUI(UI):
         self.container = ft.Column(
             controls=list_container
         )
+    def createAndLeftPage(self):
+        data = {tf.label: tf.value for tf in self.textfields if isinstance(tf, ft.TextField)}
+        print(data)
+        
+        tk = Item.getVarTable()
+        
+        for key in tk:
+            if key in data:
+                if data[key] == '': 
+                    data[key] = None
+                    continue
+        DAH.createItem(**data)
+        self.items_ui.reset_list()
+        self.page_switch()
     def get(self):
         return self.container
           
