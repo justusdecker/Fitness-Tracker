1d29dc69b83f035f16e9e5101756c04ddbd4c424
Justus <justus.d2025@gmail.com>
2026-08-29 23:13:23 +0200

    refac create page

--- src/ui/create.py
+++ src/ui/create.py
@@ -1,5 +1,6 @@
 from src.ui.ui import UI
 from src.databases.data_access import Item, DAH
+from src.databases.items import NutrientSet
 import flet as ft
 class CreateItemUI(UI):
     def __init__(self, page_switch, items_ui):
@@ -7,9 +8,38 @@ class CreateItemUI(UI):
         self.items_ui = items_ui
         self.page_switch = page_switch
         self.textfields: list[ft.TextField | ft.Button] = []
-        for key in Item.getVarTable():
-            self.textfields.append(ft.TextField(label=key))
         
+
+        for c in Item.__table__.columns:
+            if c.name in NutrientSet.__dict__:
+                print(f'is in {c.name}')
+                
+                other_objects = []
+                
+                for k in getattr(NutrientSet, c.name):
+                    other_objects.append(ft.ListTile(ft.TextField(label= k.replace('_',' ').capitalize())))
+                
+                ETS = ft.Column(
+                        spacing=0,
+                        controls=[
+                            ft.ExpansionTile(
+                                expanded=True,
+                                title=ft.Text(c.name.replace('_',' ').capitalize()),
+                                subtitle=ft.Text("Placeholder"),
+                                affinity=ft.TileAffinity.PLATFORM,
+                                maintain_state=True,
+                                collapsed_text_color=ft.Colors.BLACK,
+                                text_color=ft.Colors.BLACK_87,
+                                controls=other_objects
+                            ),
+                            
+                        ],
+                    )
+                
+                self.textfields.append(ETS)
+            else:
+                self.textfields.append(ft.TextField(label= c.name.replace('_',' ').capitalize()))
+
         self.textfields.append(
             ft.Button(content=ft.Text('Erstellen'), icon=ft.Icons.CREATE, on_click=self.createAndLeftPage)
             )
