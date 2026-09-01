ed4ad1b38fd02d77e4e948aa58a5d9e58f658984
Justus <justus.d2025@gmail.com>
2026-09-01 20:58:31 +0200

    add docstring, marked some methods as notImplementedYet

--- src/databases/data_access.py
+++ src/databases/data_access.py
@@ -2,9 +2,16 @@ from typing import List
 from src.databases.common.constants import *
 from src.databases.items import Item
 from src.databases.body import EatenLog
+from src.common.decorators import notImplementedYet
+
 class SQLAccess:
     @staticmethod
-    def close_and_dispose(s=None,e=None):
+    @notImplementedYet
+    def close_and_dispose(s=Session,e=Engine):
+        """
+        Closes the connection to the Database.
+        For resetting, imports etc.
+        """
         if s is None:
             session.close()
         else:
@@ -16,6 +23,11 @@ class SQLAccess:
             
     @staticmethod
     def connect():
+        """
+        Creates a connection to the Database
+        
+        :return: The SessionMaker, Engine & Session
+        """
         engine = create_engine(DB_URL)
         Base.metadata.create_all(engine)
         Session = sessionmaker(bind=engine)
@@ -25,24 +37,50 @@ class SQLAccess:
 class DAH:
     """
     Data Access Handler
+    
+    CRUD Methods for all needed Tables:
+    * Item
+    * EatenLog
     """
+    
+    @staticmethod
     def createItem(**data):
+        """
+        Creates an Item in the ItemTable, add to Session & commit
+        """
         obj = Item(**data)
         session.add(obj)
         session.commit()
     
+    @staticmethod
     def createEatenLogEntry(**data):
-        print(data, EatenLog.__table__.columns)
+        """
+        Creates an EatenLog in the EatenLogTable, add to Session & commit
+        """
         obj = EatenLog(**data)
         session.add(obj)
         session.commit()
         
+    @staticmethod
     def readEatenLogs() -> List[EatenLog]:
-        return session.query(EatenLog).all()
-    
-    def readItems() -> List[Item]: 
-        return session.query(Item).all()
+        """
+        Reads all EatenLogs of the EatenLogTable
         
+        :return: List[EatenLog]
+        """
+        return session.query(EatenLog).all()
+
+    @staticmethod
+    def readItems() -> List[Item]: 
+        """
+        Reads all Items of the ItemTable
+        
+        :return: List[Item]
+        """
+        return session.query(Item).all()
+    
+    @staticmethod
+    @notImplementedYet
     def updateItem(id: int, **data): 
         obj = DAH.readItem(id)
         for key in data:
@@ -53,7 +91,9 @@ class DAH:
                 key,
                 data[key]
             )
-            
+    
+    @staticmethod
+    @notImplementedYet
     def deleteItem(id: int): 
         data = session.query(Item).all()[id] #! optimize
         session.delete(data)
